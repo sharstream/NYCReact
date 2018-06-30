c490930a2e7476d7587b7a3cd0e68f3159173ff7
David Perez <david.perez@us.abb.com>
2018-06-30 19:48:25 -0400

    api express routes

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+  // gets all articles
+  getArticles: function() {
+    return axios.get("/api/articles");
+  },
+  // gets the article with given id
+  getArticle: function(id) {
+    return axios.get("/api/articles/" + id);
+  },
+  // delete the article with given id
+  deleteArticle: function(id) {
+    return axios.delete("/api/article/" + id);
+  },
+  // save an article
+  saveArticle: function(articleData) {
+    return axios.post("/api/articles", articleData);
+  }
+}
